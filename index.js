32b9c04cf340fed6e70350ff564748255a62ca9e
wescley <wescleypbpe@gmail.com>
2022-10-28 17:07:47 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+let vCb = document.getElementById("cb")
+
+
+function promocao() {
+
+  alert("Parabéns você conseguiu saltar o abismo entre a pobreza  e a riqueza. Você não é mais pobre, agora você é CABO!!!")
+  
+ 
+  }
+
+
+function mostrarImg() {
+
+vCb.className = "cLimage"
+
+
+
+}
+
+
+
+  promocao()
+  
+  mostrarImg()
